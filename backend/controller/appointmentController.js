a25f95d0bcbd86ee8c45ce0cd4466a66f5e1deb0
shivam tonpe <shivamtonpe175@gmail.com>
2025-08-07 08:53:15 +0530

    backend completed

--- backend/controller/appointmentController.js
+++ backend/controller/appointmentController.js
@@ -1,7 +1,7 @@
 import { catchAsyncErrors } from "../middlewares/catchAsyncErrors.js";
 import { ErrorHandler } from "../middlewares/errorMiddleware.js";
 import { Appointment } from "../models/appointmentSchema.js";
-import { User } from "../models/userSchema.js"; 
+import { User } from "../models/userSchema.js";
 
 export const postAppointment = catchAsyncErrors(async (req, res, next) => {
     const {
@@ -32,7 +32,6 @@ export const postAppointment = catchAsyncErrors(async (req, res, next) => {
         !department ||
         !doctor_firstName ||
         !doctor_lastName ||
-        !hasVisited ||
         !address
     ) {
         return next(new ErrorHandler("Please fill all form", 400));
@@ -52,10 +51,10 @@ export const postAppointment = catchAsyncErrors(async (req, res, next) => {
             404)
         );
     }
-   const doctorId = isconflict[0]._id;
-   const patientId = req.user._id;
-  const appointment = await Appointment.create({
-    firstName,
+    const doctorId = isconflict[0]._id;
+    const patientId = req.user._id;
+    const appointment = await Appointment.create({
+        firstName,
         lastName,
         email,
         phone,
@@ -64,7 +63,7 @@ export const postAppointment = catchAsyncErrors(async (req, res, next) => {
         gender,
         appointment_data,
         department,
-        doctor:{
+        doctor: {
             firstName: doctor_firstName,
             lastName: doctor_lastName,
         },
@@ -72,10 +71,50 @@ export const postAppointment = catchAsyncErrors(async (req, res, next) => {
         address,
         doctorId,
         patientId
-  });
+    });
 
+    res.status(200).json({
+        success: true,
+        message: "Appointment sent successfully",
+        appointment,
+    });
+});
+export const getAllAppointments = catchAsyncErrors(async (req, res, next) => {
+    const appointments = await Appointment.find();
+    res.status(200).json({
+        success: true,
+        appointments,
+    });
+});
+
+export const updateAppointmentStatus = catchAsyncErrors(async (req, res, next) => {
+    const { id } = req.params;
+    let appointment = await Appointment.findById(id);
+    if (!appointment) {
+        return next(new ErrorHandler("Appointment not found", 404));
+    }
+    appointment = await Appointment.findByIdAndUpdate(id, req.body, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false,
+    });
+    res.status(200).json({
+        success: true,
+        message: "Appointment status updated ",
+        appointment,
+    });
+   }
+);
+
+export const deleteAppointment = catchAsyncErrors(async (req, res, next) => {
+    const { id } = req.params;
+    let appointment = await Appointment.findById(id);
+    if (!appointment) {
+        return next(new ErrorHandler("Appointment not found", 404));
+    }
+   await Appointment.deleteOne();
    res.status(200).json({
        success: true,
-       message: "Appointment sent successfully",
+       message: "Appointment deleted successfully",
    });
 });
